8dd7100c07beea71eea7904f71cfc85b5e2ed184
Debasis Sahoo <dsdebasis2@gamil.com>
2024-05-02 15:43:32 +0530

    channel component updated

--- src/components/Channel/ChannelPage.jsx
+++ src/components/Channel/ChannelPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import ChannelHeading from './ChnlHeading'
-import VideoPage from '../Videos/VideoPage'
+import ChannelHeading from './ChnlHeading.jsx'
+import VideoPage from '../Videos/VideoPage.jsx'
 import { Link } from 'react-router-dom'
 import { useEffect } from "react"
 import axios from "axios"
@@ -9,13 +9,17 @@ import { useState } from 'react'
 
 const ChannelPage = () => {
 
-  const [channel,setChannel] = useState({})
+  const [channel,setChannel] = useState({
+    about:"",
+    profilePic:"",channelName:"", createdAt:"",videos:"",subscribers:""
+  })
 
   useEffect(() => {
     axios.get(`${backendUrl}/getchannel`,{
     withCredentials: true
     }).then((res)=>{
       res = res.data.data.channel
+      // console.log(res)
       setChannel(res)
     
     }).catch((err)=>{
@@ -24,9 +28,10 @@ const ChannelPage = () => {
   },[])
 
   const {about,profilePic,channelName, createdAt,videos,subscribers} = channel
+  console.log(about)
   return (
     <section className='min-h-screen w-full bg-gradient-to-br from-slate-700 to-slate-900 grid grid-flow-row  px-5'>
-      <ChannelHeading channelName={channelName} about={about} profilePic={""} createdAt={createdAt} subscribers={subscribers?.length} videos={videos?.length}/>
+      <ChannelHeading channelName={channelName} about={about || ""} profilePic={profilePic || ""} createdAt={createdAt} subscribers={subscribers?.length} videos={videos?.length}/>
       
       <VideoPage/>
     </section>
