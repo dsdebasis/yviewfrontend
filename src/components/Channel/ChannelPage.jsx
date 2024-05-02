0f8fb1b7256d04ca06a144423c65a26055f722ff
Debasis Sahoo <dsdebasis2@gamil.com>
2024-05-02 16:55:20 +0530

    channel component updated for error handeling

--- src/components/Channel/ChannelPage.jsx
+++ src/components/Channel/ChannelPage.jsx
@@ -6,34 +6,48 @@ import { useEffect } from "react"
 import axios from "axios"
 import { backendUrl } from "../index.js"
 import { useState } from 'react'
-
+import { useNavigate } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import Error from '../Error/Error.jsx'
 const ChannelPage = () => {
 
-  const [channel,setChannel] = useState({
-    about:"",
-    profilePic:"",channelName:"", createdAt:"",videos:"",subscribers:""
-  })
 
+  const [channel, setChannel] = useState({
+    
+  })
+  // about: "",
+  //   profilePic: "", channelName: "", createdAt: "", videos: "", subscribers: ""
+
+  const [error, setError] = useState({})
+  
   useEffect(() => {
-    axios.get(`${backendUrl}/getchannel`,{
-    withCredentials: true
-    }).then((res)=>{
+    axios.get(`${backendUrl}/getchannel`, {
+      withCredentials: true
+    }).then((res) => {
       res = res.data.data.channel
       // console.log(res)
       setChannel(res)
-    
-    }).catch((err)=>{
-      console.log("erorr",err)
-    })
-  },[])
 
-  const {about,profilePic,channelName, createdAt,videos,subscribers} = channel
-  console.log(about)
+    }).catch((err) => {
+      console.log("erorr", err.response.data.message)
+      setError(err.response.data)
+      // toast.erorr(err.response.data.message)
+    })
+  }, [])
+
+  const { about, profilePic, channelName, createdAt, videos, subscribers } = channel
+  // console.log(about)
+
+  const {message} = error
+  if (message ) {
+    // console.log("ds",message)
+    return <Error message={"no channel found"}/>
+  }
   return (
     <section className='min-h-screen w-full bg-gradient-to-br from-slate-700 to-slate-900 grid grid-flow-row  px-5'>
-      <ChannelHeading channelName={channelName} about={about || ""} profilePic={profilePic || ""} createdAt={createdAt} subscribers={subscribers?.length} videos={videos?.length}/>
-      
-      <VideoPage/>
+      <ChannelHeading channelName={channelName} about={about || ""} profilePic={profilePic || ""} createdAt={createdAt} subscribers={subscribers?.length} videos={videos?.length} />
+
+      <VideoPage />
     </section>
   )
 }
---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Error = ({message}) => {
+  return (
+    <div className='h-screen w-full bg-gradient-to-br from-slate-800 to-slate-950 flex justify-center items-center'>
+      
+        <span className='text-orange-200'>{message}</span>
+      
+     
+    </div>
+    
+  )
+}
+
+export default Error
