8311a06535e2b52706cd47ba2b878c0c2ae7e68f
Debasis Sahoo <dsdebasis2@gamil.com>
2024-05-11 22:51:46 +0530

    all components are updated

--- src/components/Videos/Video.jsx
+++ src/components/Videos/Video.jsx
@@ -6,7 +6,7 @@ import VideoDetails from './VideoDetails.jsx'
 const Video = () => {
 
   return (
-    <div className=' overflow-hidden rounded-xl w-full md:w-[90%] lg:w-[90%]  grid grid-flow-row  gap-y-4  md:px-0 ' >
+    <div className=' overflow-hidden rounded-xl w-full md:w-[90%] lg:w-[95%]  grid grid-flow-row  gap-y-4  md:px-0 ' >
       
       <VideoDetails/>
       <ChannelDetails/>
