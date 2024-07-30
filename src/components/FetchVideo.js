ec7bbc90be7afff6725573eb5f48630bdd430529
Debasis Sahoo <dsdebasis2@gmail.com>
2024-07-30 17:38:11 +0530

    inifnete scroll added

--- src/components/FetchVideo.js
+++ src/components/FetchVideo.js
@@ -1,22 +1,4 @@
-import axios from "axios";
-import { backendUrl } from "./index.js";
 
-
-let res, error;
-
-let FetchVideos = async function() {
-    console.log("called the function")
-  try {
-   res = await axios.get(`${backendUrl}/getvideos`)
   //  console.log("result",res.data)
-   return res.data
-  } catch (error) {
-    error = error
-    // console.log("error",error)
-    return error
-  }
-}
 
-
-
-export  {FetchVideos};
+  
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,8 @@
 import Nav from "../Navbar/Nav.jsx";
 import VideoPage from "../Videos/VideoPage.jsx";
 import Search from "../Videos/Search.jsx";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from "react";
 import axios from "axios";
 import { backendUrl } from "../index.js";
@@ -10,37 +11,52 @@ import Video from "../Videos/Video.jsx";
 import { useState } from "react";
 import VideoFooter from "../Channel/VideoFooter.jsx";
 import ChannelImg from "../Channel/ChannelImg.jsx";
-import { FetchVideos } from "../FetchVideo.js";
-
+import Loading from "../Loading/Loading.jsx";
+import { useCallback } from "react";
 const Home = () => {
-  const [videos, setVideos] = useState([]);
-  const [videoRes, setVideoRes] = useState({});
+  // const [videos, setVideos] = useState([]);
+  const [videoRes, setVideoRes] = useState([]);
   const [error, setError] = useState(false);
-  useEffect(() => {
-    // axios.get(`${backendUrl}/getvideos`).then((result) => {
-    //   result = result.data;
-    //   setVideoRes(result);
-    // }).catch((error)=>{
-    //   setError(error.response.data.message)
-    //   // console.log(error.response.data.message)
-    // })
-    
-    let getData = async () => {
-        let res = await FetchVideos()
-        // res = res?.response?.data
-        
-        // setVideoRes(res.data)
-        if(res.success == false){
-          setError(true)
-        } else{
-           setVideoRes(res)
+  let [page, setPage] = useState(1);
+  let [pageSize, setPageSize] = useState(4);
+  const [load,setLoading] = useState(false)
+  const fetchData = useCallback(() => {
+    setLoading(true)
+    axios
+      .get(`${backendUrl}/getvideos/${page}/${pageSize}`)
+      .then((res) => {
+       
+        if (error == false) {
+         setVideoRes((prev)=>[...prev,...res.data.data]) 
+         setLoading(false)
         }
-        
-    };
-    getData()
-   
-  }, []);
+      })
+      .catch((error) => {
+        console.log("error while fetching", error);
+        setError(error);
+        setLoading(false)
+      });
+  });
 
+  const handleScroll = (e) => {
+    console.log("scrolled");
+    if (
+      window.innerHeight + document.documentElement.scrollTop >=
+        document.documentElement.offsetHeight - 5 &&
+      !error
+    ) {
+      setPage((prev) => prev + 1);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+  useEffect(() => {
+    
+    window.addEventListener("scrollend", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <section className="min-h-screen px-2  box-border bg-gradient-to-r from-slate-900 to-slate-700 font-sans pt-2">
       <Nav />
@@ -50,29 +66,24 @@ const Home = () => {
 
       {/* <VideoPage /> */}
       <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4  place-items-center gap-y-6  lg:gap-y-5 text-white py-2">
-        {error ? (
-          <h1 className=" w-full text-center text-yellow-500">
-            No Videos Found
-          </h1>
-        ) : (
-          <></>
-        )}
-
-        {videoRes?.data?.map((item, index) => {
+       
+        {videoRes?.map((item) => {
           return (
             <Video
-              key={item._id}
-              thumbnail={item.thumbnail}
-              vid={item._id}
-              src={item?.videoFile}
-              title={item.title}
-              uploadTime={item.uploadTime}
-              videoOwner={item.ownerName}
-              duration={item.duration}
-              channelProfilePic={item.channelProfilePic}
+            key={item._id}
+            thumbnail={item.thumbnail}
+            vid={item._id}
+            src={item?.videoFile}
+            title={item.title}
+            uploadTime={item.uploadTime}
+            videoOwner={item.ownerName}
+            duration={item.duration}
+            channelProfilePic={item.channelProfilePic}
             />
           );
         })}
+        {error ?<h1 className="text-yellow-500 ">No More Video Found</h1>:<></>}
+       
       </section>
       <ToastContainer />
     </section>
