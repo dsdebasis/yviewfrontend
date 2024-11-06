ff6d8a04a28840db76507bff8f6130ec2647ad07
Debasis Sahoo <dsdebasis2@gmail.com>
2024-11-06 12:36:33 +0530

    comment component added

--- src/components/Comments/CommentWrap.jsx
+++ src/components/Comments/CommentWrap.jsx
@@ -3,11 +3,9 @@ import Comments from "./Comments.jsx";
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
-
 import { backendUrl } from "../index.js";
 import AddComment from "./AddComment.jsx";
-// import { useContext } from "react";
-// import { CmntContext } from "../../Context/Context.js";
+
 import {  Toaster } from "react-hot-toast";
 import { CommentEditOpnProvider } from "../../Context/CommentEditOpnProvider.jsx";
 
@@ -16,34 +14,31 @@ function CommentWrap({ videoid }) {
   const [pageSize, setPageSize] = useState(4);
   const [comments, setComments] = useState([]);
 
-  // const {cmnt} =useContext(CmntContext)
 
   useEffect(() => {
     axios
       .get(`${backendUrl}/comments/${videoid}/${page}/${pageSize}`)
       .then((res) => {
-        console.log("comments", res.data.message.comments.length);
-        // setComments(res.data.message.comments);
-      })
-      .catch((error) => {});
+        setComments(res.data.message.comments);
+      }).catch((error) => {});
   }, []);
+
+
   return (
     <section className="min-h-screen h-auto  w-full px-2 pb-3 mt-10  md:px-2 lg:px-3 ">
       <AddComment />
       <CommentEditOpnProvider>
         <section className="grid grid-flow-row gap-y-4">
-          {comments.map((cmnt) => {
+           {comments?.map((i, d) => {
             return (
               <Comments
-                key={cmnt._id}
-                id={cmnt._id}
-                content={cmnt.comment}
-                like={""}
-                dislike={""}
-                username={cmnt.username}
-                commentTime={cmnt.commentTime}
-                comments={comments}
-                setComments={setComments}
+                key={i._id}
+                id={i._id}
+                username={i.username}
+                content={i.comment}
+                commentTime={i.commentTime}
+                like={i.like}
+                dislike={i.dislike}
               />
             );
           })}
--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,40 +1,36 @@
 import React from "react";
 import { SlLike } from "react-icons/sl";
 import { SlDislike } from "react-icons/sl";
-
 import { CgProfile } from "react-icons/cg";
 import CommentEditOption from "./CommentEditOption.jsx";
 import CommentContext from "../../Context/CommentContext.jsx";
-// import CommentProfile from "./CommentProfile.jsx";
-import { CmntContext } from "../../Context/Context.js";
-import { useContext } from "react";
+
 function Comments(props) {
 
-  const { cmnt, setCmnt } = useContext(CmntContext);
-  setCmnt(props);
+  
   return (
     <CommentContext>
-      <section id={cmnt.id}>
+      <section id={props.id}>
         <CgProfile className="text-3xl text-white  lg:text-4xl mt-2" />
         <div className="max-w-fit bg-gradient-to-tr  from-slate-500 rounded-2xl py-3 px-3 mt-3 text-white ">
           <h1 className=" text-green-300 text-sm capitalize">
-            {cmnt.username || "User"}
-            <span className="text-slate-300 ml-4">{cmnt.commentTime}</span>
+            {props.username || "User"}
+            <span className="text-slate-300 ml-4">{props.commentTime}</span>
           </h1>
           <p className="row-span-2 w-full text-slate-200  tracking-wider text-justify p-1 text-sm lg:text-base content">
-            {cmnt.content || ""}
+            {props.content || ""}
           </p>
           <div className="flex  gap-x-5 mt-2 lg:gap-x-9  max-h-fit">
             <button>
               <SlLike className="hover:scale-125 transition-all hover:text-blue-500" />
-              <span>{cmnt.like}</span>
+              <span>{props.like}</span>
             </button>
             <button>
               <SlDislike className="hover:scale-125 transition-all hover:text-blue-500" />
-              <span>{cmnt.dislike}</span>
+              <span>{props.dislike}</span>
             </button>
           </div>
-          <CommentEditOption id={cmnt.id} />
+          <CommentEditOption id={props.id} />
         </div>
       </section>
     </CommentContext>
